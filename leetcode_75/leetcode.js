536041fcf3d7c53f611e5cc6490fe0cc46ca2cc8
Andrew <andrewvo1024@gmail.com>
2024-08-12 20:30:21 -0500

    1768 solved

--- leetcode_75/leetcode.js
+++ leetcode_75/leetcode.js
@@ -51,4 +51,16 @@ var reverseVowels = function(s) {
 };
 
 // -----------------------------------------------------------------------------
+// 1768. Merge Strings Alternately
 
+var mergeAlternately = function(word1, word2) {
+    // create an empty string
+    let result = ''
+
+    for (let i=0; i < Math.max(word1.length, word2.length); i++) {
+        if (i < word1.length) result += word1[i]
+        if (i < word2.length) result += word2[i]
+    }
+
+    return result
+};
