3183f90ee9f36e5f7e95a1e5a8254882255a189a
Andrew <andrewvo1024@gmail.com>
2024-08-13 20:06:56 -0500

    solved

--- leetcode_75/leetcode.js
+++ leetcode_75/leetcode.js
@@ -66,3 +66,22 @@ var mergeAlternately = function(word1, word2) {
 };
 
 // -----------------------------------------------------------------------------
+// 1071. Greatest Common Divisor of Strings
+
+// THIS ONE IS CONFUSING AS HECK. Use MODULO.
+var gcdOfStrings = function(str1, str2) {
+    // handle the base case
+    if (str1 + str2 !== str2 + str1) return '';
+    let a = str1.length
+    let b = str2.length
+
+    // loop (divide) until you find the
+    // highest common factor (length of string)
+    // like we did in maths
+    while (b) {
+        let temp = b
+        b = a % b
+        a = temp
+    }
+    return str1.substring(0, a)
+}
